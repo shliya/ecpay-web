51fb64ffe02b6583ee57d9886a27bac16e2fd1d3
shiliyahamster <shiliyahamster@gmail.com>
2025-05-13 16:51:11 +0800

    fix db

--- server/config/database.js
+++ server/config/database.js
@@ -2,33 +2,26 @@ require('dotenv').config();
 
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        port: 6543,
-        dialect: 'postgres',
-        logging: true,
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false,
-            },
-            // Transaction Pooler 特定設定
-            application_name: 'transaction_pooler',
-            statement_timeout: 60000,
-            idle_in_transaction_session_timeout: 60000,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    logging: true,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
         },
-        pool: {
-            max: 10,
-            min: 0,
-            acquire: 30000,
-            idle: 10000,
-        },
-    }
-);
+        // Transaction Pooler 特定設定
+        application_name: 'transaction_pooler',
+        statement_timeout: 60000,
+        idle_in_transaction_session_timeout: 60000,
+    },
+    pool: {
+        max: 10,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+    },
+});
 
 sequelize
     .authenticate()
